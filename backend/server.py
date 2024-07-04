5d8cb517a496156c59deab3d1a50ac5b0b9163b9
codrinatus <66734258+codrinatus@users.noreply.github.com>
2024-07-05 00:46:01 +0300

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -282,5 +282,5 @@ def serve_react_app(path):
     else:
         return send_from_directory(app.static_folder, 'index.html')
 
-port = int(os.environ.get('PORT', 80))
+port = int(os.environ.get('PORT', 8080))
 app.run(host='0.0.0.0', port=port)
