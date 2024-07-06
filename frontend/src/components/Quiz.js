d2db95b5f66d1cba0ef27051daddaf0484f2b848
codrinatus <66734258+codrinatus@users.noreply.github.com>
2024-07-06 10:41:00 +0300

    Update Quiz.js

--- frontend/src/components/Quiz.js
+++ frontend/src/components/Quiz.js
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import axios from 'axios';
-// axios.defaults.baseUrl = "http://localhost:3001/"
 
 function Quiz() {
     const { level } = useParams();
